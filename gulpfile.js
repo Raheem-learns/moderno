a3add1b0af42c4042125586be33820b94fd57adc
Raheem-learn <rustammir.work@gmail.com>
2021-06-19 04:30:58 +0500

    made a header

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 'use strict';
 const { dest,src,watch, parallel, series } = require('gulp');
-const scss                         = require('gulp-sass');
-const concat                       = require('gulp-concat');
-const browserSync                  = require('browser-sync').create();
-const uglify                       = require('gulp-uglify-es').default;
-const autoPrefixer                 = require('gulp-autoprefixer');
-const imageMin                     = require('gulp-imagemin');
-const del                          = require('del')
+const scss                                 = require('gulp-sass');
+const concat                               = require('gulp-concat');
+const browserSync                          = require('browser-sync').create();
+const uglify                               = require('gulp-uglify-es').default;
+const autoPrefixer                         = require('gulp-autoprefixer');
+const imageMin                             = require('gulp-imagemin');
+const del                                  = require('del')
+const cssmin                               = require('gulp-cssmin');
 
 let paths = {
   styles: {
@@ -23,10 +24,20 @@ let paths = {
 };
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    paths.scripts.src
+     paths.scripts.src
   ])
   .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(dest(paths.scripts.dest))
+  .pipe(browserSync.stream())
+}
+
+function jsLibs() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+  ])
+  .pipe(concat('libs.min.js'))
   .pipe(uglify())
   .pipe(dest(paths.scripts.dest))
   .pipe(browserSync.stream())
@@ -38,7 +49,7 @@ function browsersync(){
     }
   });
 }
-function styles() {
+function sass() {
   return src(paths.styles.src)
     .pipe(scss({outputStyle: 'compressed'}))
     .pipe(concat('style.min.css'))
@@ -49,6 +60,14 @@ function styles() {
     .pipe(dest(paths.styles.dest))
     .pipe(browserSync.stream())
 };
+
+function style() {
+  return src(['node_modules/normalize.css/normalize.css',
+            'node_modules/slick-carousel/slick/slick.css'])
+    .pipe(concat('libs.min.css'))
+    .pipe(cssmin())
+    .pipe(dest(paths.styles.dest))
+}
 
 function images() {
   return src(['src/images/**/*'])
@@ -68,7 +87,7 @@ function images() {
 }
 
 function watching() {
-  watch([paths.styles.src],styles)
+  watch([paths.styles.src],sass)
   watch([paths.scripts.src],scripts)
   watch(paths.markup.src).on('change', browserSync.reload)
 };
@@ -81,17 +100,21 @@ function build() {
   return src([
     'src/*.html',
     'src/css/style.min.css',
+    'src/css/libs.min.css',
     'src/fonts/**/*',
-    'src/js/main.min.js'
+    'src/js/main.min.js',
+    'src/js/libs.min.js'
   ],{base: 'src'})
   .pipe(dest('dist'))
 }
 
-exports.styles = styles;
+exports.jsLibs = jsLibs;
+exports.style = style;
+exports.sass = sass;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.default = parallel(browsersync,watching);
-exports.build = series(delDist,images,build);
+exports.build = series(delDist,images,style,build);
 exports.images = images;
 exports.delDist = delDist;
